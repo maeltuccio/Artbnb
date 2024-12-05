7d8bbb58a5c7073fffcaea299952ffedab6b92b5
(martin selle) (atelier.martin.selle@gmail.com) <atelier.martin.selle@gmail.com>
2024-12-05 17:20:24 +0100

    FuckingSCSS

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -4,6 +4,7 @@ class ArtworksController < ApplicationController
 
   def index
     @artworks = Artwork.all
+    @banner_image = random_banner_image
   end
 
   def new
@@ -49,5 +50,14 @@ class ArtworksController < ApplicationController
   def artwork_params
     params.require(:artwork).permit(:title, :description, :price, :image)
   end
-end
 
+  def random_banner_image
+    banner_images = [
+      "https://res.cloudinary.com/djqnyh6hr/image/upload/v1733407765/bwl4yx4ojjd5xrrju483549hoogy_q4e6yu.jpg",
+      "https://res.cloudinary.com/djqnyh6hr/image/upload/v1733407750/utxgxwtkuqe0zct4e8sz7k3xpmnm_idaxcp.jpg",
+      "https://res.cloudinary.com/djqnyh6hr/image/upload/v1733407749/vht7m88pm72e55wfuvjna0tfrcao_xtpkyw.jpg",
+      "https://res.cloudinary.com/djqnyh6hr/image/upload/v1733407750/utxgxwtkuqe0zct4e8sz7k3xpmnm_idaxcp.jpg"
+    ]
+    banner_images.sample
+  end
+end
